56e5d44986d65c9c4139e0816030dce246a2c0bc
rfahlevi <rezafahlevi55@gmail.com>
2020-04-17 09:50:14 +0700

    Single Product

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$('#handleCounter').handleCounter({
+    minimum: 1,
+    maximum: null,
+})
+
+$('#handleCounter').handleCounter({
+    onChange:function(){},
+    onMinimun:function(){},
+    onMaximum:function(){}
+})
